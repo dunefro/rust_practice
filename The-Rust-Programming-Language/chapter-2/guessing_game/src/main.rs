ca14e780c1990e2078a18f321830bf48fa50ad64
Vedant Pareek <dunefro@gmail.com>
2022-10-20 14:21:04 +0530

    display of secret number removed

--- The-Rust-Programming-Language/chapter-2/guessing_game/src/main.rs
+++ The-Rust-Programming-Language/chapter-2/guessing_game/src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 fn main() {
     println!("Guess the number");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is {secret_number}");
+    // println!("The secret number is {secret_number}");
 
     // for looping
     loop {
